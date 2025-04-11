4edc4472897ae7f202680ae2771a860499385999
danielfleshler <daniel.fle@my.hit.ac.il>
2025-04-11 22:15:44 +0300

    minor fixes

--- client/src/utils/ProtectedRoutes.jsx
+++ client/src/utils/ProtectedRoutes.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 const ProtectedRoutes = () => {
-	const isAuthenticated = useContext(AuthContext);
+	const { isAuthenticated } = useContext(AuthContext);
 	return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
