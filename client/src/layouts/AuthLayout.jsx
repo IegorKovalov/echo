41a9db5a46d9d976ee27f17513bca62e24073ba9
danielfleshler <daniel.fle@my.hit.ac.il>
2025-05-02 12:32:25 +0300

    fixed auth layout

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
 	return (
 		<div className="min-h-screen bg-black text-white d-flex flex-column">
-			<div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center p-4">
+			<div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
 				<Outlet />
 			</div>
 		</div>
