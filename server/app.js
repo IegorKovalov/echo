7f331f17b578b6b0421a51f345a53006eb4f2060
danielfleshler <daniel.fle@my.hit.ac.il>
2025-05-18 19:13:23 +0300

    roomRouter in server

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const dotenv = require("dotenv");
 const userRouter = require("./routes/userRoutes");
 const postRouter = require("./routes/postRoutes");
 const followerRouter = require("./routes/followerRoutes");
+const roomRouter = require("./routes/roomRoutes");
 
 dotenv.config();
 
@@ -35,5 +36,6 @@ app.get("/", (req, res) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/followers", followerRouter);
+app.use("/api/v1/rooms", roomRouter);
 
 module.exports = app;
